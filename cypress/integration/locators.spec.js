ba3ff96e3246e62e6192b18a143ec50193425b3c
Laryssa Ribeiro <LaryssaFernandaR@hotmail.com>
2022-06-10 11:27:24 -0300

    locators

---
+++ cypress/integration/locators.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+describe('Work with alerts',() => {
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })//Em todos os Tests deve conter boas praticas no codigo "BEFORE all"
+
+    beforeEach(() => {
+        cy.reload()
+    })//Em todos os Tests deve conter boas praticas no codigo "BEFORE EACH"
+
+    it('using jquery selector', () => {
+        cy.get(':nth-child(1) > :nth-child(3) > [type="button"]')
+        cy.get('table#tabelaUsuarios tbody > tr:eq(0) td:nth-child(3) > input ')
+        cy.get("[onclick*='Francisco']")
+        cy.get('#tabelaUsuarios td:contains(\'Doutorado\'):eq(0) ~ td:eq(3) > input')
+        cy.get('#tabelaUsuarios tr:contains(\'Doutorado\'):eq(0) td:eq(6) > input')//busncando por linha 
+
+    })//padrão de busca elemento
+
+    it('using xpath', () =>{
+
+        cy.xpath('//input')
+    })
+
+})
+
