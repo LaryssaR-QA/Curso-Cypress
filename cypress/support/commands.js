765db09c73603276aa7478b3f416891ddd32c2af
Laryssa Ribeiro <LaryssaFernandaR@hotmail.com>
2022-07-04 23:40:23 -0300

    Funcional

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,9 +24,27 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
+import loc from './locators'
+
 Cypress.Commands.add('clickAlert', (locator, message) => {
     cy.get(locator).click()
     cy.on('window:alert', msg => {
         expect(msg).to.be.equal(message)
     })
+})
+
+Cypress.Commands.add('login', (user, passwd) => {
+    cy.visit('http://barrigareact.wcaquino.me/')
+
+    cy.get(loc.LOGIN.USER).type('LaryssaFernandaR@hotmail.com')
+    cy.get(loc.LOGIN.PASSWORD).type('12346')
+
+    cy.get(loc.LOGIN.BTN_LOGIN).click()
+
+    cy.get(loc.MESSAGE).should('contain', 'Bem vindo')//validation login
+})
+
+Cypress.Commands.add('resetApp', () => {
+    cy.get(loc.MENU.SETTINGS).click()
+    cy.get(loc.MENU.RESET).click()
 })
