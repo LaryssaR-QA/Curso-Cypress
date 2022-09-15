62738c98177c4d0e9a35fd43ee3464e6977787c4
laryssaR-QA <laryssafernandar@hotmail.com>
2022-09-15 17:37:52 -0300

    Should get balance

--- cypress/e2e/new system/backend.cy.js
+++ cypress/e2e/new system/backend.cy.js
@@ -29,64 +29,115 @@ describe('Should test at a functional level', () => {
             expect(res.status).to.be.equal(201)
             expect(res.body).to.have.property('id')
             expect(res.body).to.have.property('nome', 'Conta via rest')
-        })    
-})
+        })   
+    })// end create
 
-it('update an account', () => {
-    cy.getContasByName('Conta para alterar')
-    .then(contaId => {
+    it('update an account', () => {
+        cy.getContasByName('Conta para alterar')
+        .then(contaId => {
+            cy.request({
+                url: `contas/${contaId}`,
+                method: 'PUT',
+                headers: { Authorization: `JWT ${token}` },
+                body: {
+                    nome: 'onta alterada via rest'
+                }
+            }).as('response')
+        })
+        cy.get('@response').its('status').should('be.equal',200)
+    })//end update
+
+    it('Should not create an account with same name', () => {
         cy.request({
-            url: `contas/${contaId}`,
-            method: 'PUT',
-            headers: { Authorization: `JWT ${token}` },
-            body: {
-                nome: 'onta alterada via rest'
-            }
-        }).as('response')
-    })
-    cy.get('@response').its('status').should('be.equal',200)
-})
-
-it('Should not create an account with same name', () => {
-    cy.request({
-        url: '/contas',
-        method: 'POST',
-        headers: { Authorization: `JWT ${token}` },
-        body: {
-            nome: 'Conta mesmo nome',
-            },
-
-        failOnStatusCode: false  //validar testes com erro
-
-        }).as('response')
-        
-    cy.get('@response').then(res => {
-        console.log(res)
-        expect(res.status).to.be.equal(400)
-        expect(res.body.error).to.have.equal('Já existe uma conta com esse nome!')
-    })
-})
-
-it('should create a transaction', () => {
-    cy.getContasByName('Conta para movimentacoes')
-    .then(contaId => {
-        cy.request({
-            url: '/transacoes',
+            url: '/contas',
             method: 'POST',
             headers: { Authorization: `JWT ${token}` },
             body: {
-                conta_id: contaId,
-                data_pagamento: dayjs().format('DD/MM/YYYY'), // formatacao data 1 dia atualizado
-                data_transacao: dayjs().format('DD/MM/YYYY'), //formatação data atualizada
-                descricao: "desc",
-                envolvido: "inter",
-                status: true,
-                tipo: "REC", //receita
-                valor: "6000"
-            }   
-        }).as('response') 
-     })
-     cy.get('@response').its('status').should('be.equal', 201)  
-     cy.get('@response').its('body.id').should('exist')
+                nome: 'Conta mesmo nome',
+                },
+
+            failOnStatusCode: false  //validar testes com erro
+
+            }).as('response')
+            
+        cy.get('@response').then(res => {
+            console.log(res)
+            expect(res.status).to.be.equal(400)
+            expect(res.body.error).to.have.equal('Já existe uma conta com esse nome!')
+        })
     })
-})
+
+    it('should create a transaction', () => {
+        cy.getContasByName('Conta para movimentacoes')
+        .then(contaId => {
+            cy.request({
+                url: '/transacoes',
+                method: 'POST',
+                headers: { Authorization: `JWT ${token}` },
+                body: {
+                    conta_id: contaId,
+                    data_pagamento: dayjs().format('DD/MM/YYYY'), // formatacao data atualizada
+                    data_transacao: dayjs().format('DD/MM/YYYY'), //formatação data atualizada
+                    descricao: "desc",
+                    envolvido: "inter",
+                    status: true,
+                    tipo: "REC", //receita
+                    valor: "6000"
+                }   
+            }).as('response') 
+        })
+        cy.get('@response').its('status').should('be.equal', 201)  
+        cy.get('@response').its('body.id').should('exist')
+    })
+
+    it.only('Should get balance', () => {
+        cy.request({
+            url: '/saldo',
+            method: 'GET',
+            headers: { Authorization: `JWT ${token}`}
+            }).then(res => {
+            let saldoConta = null //var
+
+            res.body.forEach(c => {
+                if(c.conta == 'Conta para saldo') saldoConta = c.saldo
+            })
+            expect(saldoConta).to.be.equal('534.00')
+        })
+
+        cy.request({
+            method: 'GET',
+            url: '/transacoes',
+            headers: { Authorization: `JWT ${token}`},
+            qs: {descricao: 'Movimentacao 1, calculo saldo'}
+        }).then(res => {
+            cy.request({
+                url: `/transacoes/${res.body[0].id}`,
+                method: 'PUT',
+                headers: { Authorization: `JWT ${token}`},
+                body: {
+                    status: true,
+                    data_transacao: dayjs(res.body[0].data_transacao).format('DD/MM/YYYY'),
+                    data_pagamento: dayjs(res.body[0].data_pagamento).format('DD/MM/YYYY'),
+                    descricao: res.body[0].descricao,
+                    envolvido: res.body[0].envolvido,
+                    valor: res.body[0].valor,
+                    conta_id: res.body[0].conta_id
+                }
+            }).its('status').should('be.equal', 200)
+        })
+        
+        cy.request({
+            url: '/saldo',
+            method: 'GET',
+            headers: { Authorization: `JWT ${token}`}
+            }).then(res => {
+            let saldoConta = null //var
+
+            res.body.forEach(c => {
+                if(c.conta == 'Conta para saldo') saldoConta = c.saldo
+            })
+            expect(saldoConta).to.be.equal('4034.00')
+        })
+    })
+
+})//Describe
